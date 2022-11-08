b4bb62d72279c77e88d454d3b6a39fa8b718d10a
dekarya123 <arya.satyad123@gmail.com>
2022-11-08 15:27:18 +0800

    Add component and layout

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>2022 &copy; Foodify Team</p>
+    </div>
+  );
+};
+
+export default Footer;
