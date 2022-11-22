10155a29d785af88ba9db3eb61df8c33aec2b622
dekarya123 <arya.satyad123@gmail.com>
2022-11-22 18:15:41 +0800

    fix responsive layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
-import Popular from "../components/recipes/Popular";
+import Popular from '../components/recipes/Popular';
 
-import React from "react";
-import Footer from "../components/layout/Footer";
+import React from 'react';
+import Footer from '../components/layout/Footer';
 
 function HomePage() {
   return (
