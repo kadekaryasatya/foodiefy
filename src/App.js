749fe4c636f39d42e4514ec9aa2c190784c081da
dekarya123 <arya.satyad123@gmail.com>
2022-11-28 16:18:44 +0800

    Add route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import Detail from './pages/Detail';
-import Footer from './components/layout/Footer';
-import About from './pages/About';
-// import Pages from "./pages/Pages";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className='app'>
-      <About />
-      <Footer />
-    </div>
-  );
+import HomePage from './pages/HomePage';
+import AddPage from './pages/AddPage';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authedUser: null,
+    };
+  }
+  render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className='contact-app'>
+          <header className='contact-app__header'>
+            <h1>Aplikasi Kontak</h1>
+          </header>
+          <main>
+            <Routes>
+              <Route path='/*' element={<p>Halaman Login</p>} />
+              <Route path='/register' element={<p>Halaman Register</p>} />
+            </Routes>
+          </main>
+        </div>
+      );
+    }
+
+    return (
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/add' element={<AddPage />} />
+      </Routes>
+    );
+  }
 }
 
 export default App;
