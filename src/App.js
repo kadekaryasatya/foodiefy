15ad47f4eb108065b8a4316bcdf252d69d06ec85
dekarya123 <arya.satyad123@gmail.com>
2022-11-29 11:34:58 +0800

    Add route and validation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { getUserLogged, putAccessToken } from './utils/api';
 
 import HomePage from './pages/HomePage';
 import AddPage from './pages/AddPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import About from './pages/About';
+import MyRecipes from './pages/MyRecipes';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,28 +15,41 @@ class App extends React.Component {
 
     this.state = {
       authedUser: null,
+      initializing: true,
     };
+    this.onLoginSuccess = this.onLoginSuccess.bind(this);
   }
+
+  async onLoginSuccess({ accessToken }) {
+    putAccessToken(accessToken);
+    const { data } = await getUserLogged();
+
+    this.setState(() => {
+      return {
+        authedUser: data,
+      };
+    });
+  }
+
   render() {
+    if (this.state.initializing) {
+      return null;
+    }
     if (this.state.authedUser === null) {
       return (
-        <div className='contact-app'>
-          <header className='contact-app__header'>
-            <h1>Aplikasi Kontak</h1>
-          </header>
-          <main>
-            <Routes>
-              <Route path='/*' element={<p>Halaman Login</p>} />
-              <Route path='/register' element={<p>Halaman Register</p>} />
-            </Routes>
-          </main>
-        </div>
+        <Routes>
+          <Route path='/*' element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
       );
     }
 
     return (
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/myrecipes' element={<MyRecipes />} />
         <Route path='/add' element={<AddPage />} />
       </Routes>
     );
