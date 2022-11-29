05997fc09fdce87f0ac01d910d527363d2404872
dekarya123 <arya.satyad123@gmail.com>
2022-11-29 11:34:36 +0800

    Add add page

--- src/pages/AddPage.jsx
+++ src/pages/AddPage.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { addNote } from '../utils/api';
+import { useNavigate } from 'react-router-dom';
+import useInput from '../hooks/useInput';
+import './AddPage.css';
 
 function AddPage() {
-  return <h1>ini Halaman Add</h1>;
+  const [title, onTitleChange] = useInput('');
+  const [body, onBodyChange] = useInput('');
+  const limit = 50;
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    /**
+     * Addnote
+     */
+    addNote({ title, body }).then((res) => {
+      if (!res.error) {
+        navigate('/');
+      }
+    });
+  };
+
+  return (
+    <div className='form-input' id='form-input'>
+      <h2>Add Recipe</h2>
+      <form className='note-input' onSubmit={handleSubmit}>
+        <input className='note-input__title' type='text' placeholder='Recipe Title' value={title} id='title' onChange={onTitleChange} required />
+        <p className='note-input__title__char-limit'>{limit - title.length} remaining character</p>
+        <textarea placeholder='Instruction or Ingredient' value={body} onChange={onBodyChange} required />
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddPage;
