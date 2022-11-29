0014fa8bf08bbbcb70825c731160f20b12674d11
dekarya123 <arya.satyad123@gmail.com>
2022-11-29 14:52:32 +0800

    Add link pages

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ name, logout }) => {
+  // const { pathname } = useLocation();
+
   return (
-    <div className="Navbar">
-      <p>Ini Navbar</p>
+    <div className='Navbar'>
+      <ul>
+        <li>
+          <Link to='/' className='home-nav' title='home'>
+            <u>Home</u>
+          </Link>
+        </li>
+        <li>
+          <Link to='/myrecipes' title='MyRecipes'>
+            <u>MyRecipes</u>
+          </Link>
+        </li>
+        <li>
+          <Link to='/about' className='about-nav' title='about'>
+            <u>About Us</u>
+          </Link>
+        </li>
+
+        <li>
+          <Link to='/' className='logout' onClick={logout} title='logout'>
+            <u>{name}</u>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
