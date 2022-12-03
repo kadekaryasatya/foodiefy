743320ae6ab59a648b44f131de1cdc94b98caa41
dekarya123 <arya.satyad123@gmail.com>
2022-12-03 22:23:48 +0800

    Add delte button

---
+++ src/components/myRecipes/DeleteButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AiFillDelete } from 'react-icons/ai';
+
+function DeleteButton({ id, onDelete }) {
+  return (
+    <button className='note-item__delete-button' title='Delete' onClick={() => onDelete(id)}>
+      <AiFillDelete />
+    </button>
+  );
+}
+
+DeleteButton.propTypes = {
+  id: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+export default DeleteButton;
