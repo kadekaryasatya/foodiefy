41cd84d24effcef580decf4f3d685e2f1169810a
dekarya123 <arya.satyad123@gmail.com>
2022-12-04 14:06:34 +0800

    fix footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,51 +1,52 @@
 import React from 'react';
 import './Footer.css';
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
-
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <div className='footer container mt-5 w-100'>
-      <div className="row w-100 justify-content-center">
-        <div className="col-6">
+      <div className='row w-100 justify-content-center'>
+        <div className='col-6'>
           <h3>Foodiefy</h3>
-          <p className='text-secondary'>Everything you need about finding <br/>
-            recipe will be here, where it will be easier <br/>
-            for you</p>
-          <div className="icon-list">
+          <p className='text-secondary'>
+            Everything you need about finding <br />
+            recipe will be here, where it will be easier <br />
+            for you
+          </p>
+          <div className='icon-list'>
             <FaFacebookF></FaFacebookF>
             <FaTwitter></FaTwitter>
             <FaInstagram></FaInstagram>
           </div>
         </div>
-        <div className="col-6 footer-link">
-          <div className="row">
-            <div className="col-4">
+        <div className='col-6 footer-link'>
+          <div className='row'>
+            <div className='col-4'>
               <h4>Recipe</h4>
               <ul>
-                <li>Rice</li>
-                <li>Bread</li>
-              </ul>
-            </div>
-            <div className="col-4">
-              <h4>Product</h4>
-              <ul>
-                <li>New</li>
                 <li>Popular</li>
-                <li>Most Read</li>
+                <li>Vegetarian</li>
+                <li>Random</li>
               </ul>
             </div>
-            <div className="col-4">
+            <div className='col-4'>
+              <h4>Menu</h4>
+              <ul>
+                <li>Home</li>
+                <li>MyRecipes</li>
+                <li>About Us</li>
+              </ul>
+            </div>
+            <div className='col-4'>
               <h4>Contact</h4>
               <ul>
-                <li>jalan in aja dlu</li>
+                <li>Foodiefy team</li>
                 <li>+62 1234 7645</li>
-                <li>example@email.com</li>
+                <li>foodiefy@gmail.com</li>
               </ul>
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
