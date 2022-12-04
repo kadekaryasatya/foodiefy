0d0e8caf9d85d5b42a4219d5bff8f5689824ba1f
dekarya123 <arya.satyad123@gmail.com>
2022-12-04 14:18:11 +0800

    Styling Not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='not-found'>
       <h1>404</h1>
       <h2>Page Not Found</h2>
     </div>
