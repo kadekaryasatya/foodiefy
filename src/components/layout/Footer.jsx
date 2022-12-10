22d5e60a1b51f100e8e798f73a27713bbb229fef
dekarya123 <arya.satyad123@gmail.com>
2022-12-10 20:48:22 +0800

    Add Link Footer menu

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,43 +1,56 @@
-import React from "react";
-import "./Footer.css";
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className="footer container mt-5 w-100">
-      <div className="footer-resp row w-100 justify-content-center">
-        <div className="footer-text col-lg-6 mb-3 col-sm-12 col-md-12">
+    <div className='footer container mt-5 w-100'>
+      <div className='footer-resp row w-100 justify-content-center'>
+        <div className='footer-text col-lg-6 mb-3 col-sm-12 col-md-12'>
           <h3>Foodiefy</h3>
-          <p className="text-secondary">
+          <p className='text-secondary'>
             Everything you need about finding <br />
             recipe will be here, where it will be easier <br />
             for you
           </p>
-          <div className="icon-list">
+          <div className='icon-list'>
             <FaFacebookF></FaFacebookF>
             <FaTwitter></FaTwitter>
             <FaInstagram></FaInstagram>
           </div>
         </div>
-        <div className="col-lg-6 col-sm-12 col-md-12 footer-link">
-          <div className="row">
-            <div className="col-4">
+        <div className='col-lg-6 col-sm-12 col-md-12 footer-link'>
+          <div className='row'>
+            <div className='col-4'>
               <h4>Recipe</h4>
               <ul>
-                <li>Popular</li>
-                <li>Vegetarian</li>
-                <li>Random</li>
+                <Link to='/explore'>
+                  <li>Popular</li>
+                </Link>
+                <Link to='/explore'>
+                  <li>Vegetarian</li>
+                </Link>
+                <Link to='/explore'>
+                  <li>Random</li>
+                </Link>
               </ul>
             </div>
-            <div className="col-4">
+            <div className='col-4'>
               <h4>Menu</h4>
               <ul>
-                <li>Home</li>
-                <li>MyRecipes</li>
-                <li>About Us</li>
+                <Link to='/'>
+                  <li>Home</li>
+                </Link>
+                <Link to='/myrecipes'>
+                  <li>MyRecipes</li>
+                </Link>
+                <Link to='/about'>
+                  <li>About Us</li>
+                </Link>
               </ul>
             </div>
-            <div className="col-4">
+            <div className='col-4'>
               <h4>Contact</h4>
               <ul>
                 <li>Foodiefy team</li>
